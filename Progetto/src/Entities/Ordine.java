27726430f93243089e36308d8cbba7eb7933c975
Riccardo8991 <75617080+Riccardo8991@users.noreply.github.com>
2020-12-28 19:15:40 +0100

    update Ordine.java

---
+++ Progetto/src/Entities/Ordine.java
@@ -0,0 +1,21 @@
+package Entities;
+
+public class Ordine {
+	private Integer idOrdine;
+	private java.sql.Timestamp creazioneOrdine;
+	private java.sql.Timestamp inizioConsegna;
+	private java.sql.Timestamp fineConsegna;
+	private float totale;
+	private Rider rider;
+	
+
+	public Ordine(Integer idOrdine, java.sql.Timestamp creazioneOrdine, float totale, Rider rider) {
+		super();
+		this.idOrdine = idOrdine;
+		this.creazioneOrdine = creazioneOrdine;
+		this.totale = totale;
+		this.rider = rider;
+	}
+	
+	}
+
